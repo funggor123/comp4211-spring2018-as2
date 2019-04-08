2ef6cc2e446105e5eb2783cd42ff323ccff25a4f
s31b18 <funghiantie@gmail.com>
2019-04-08 21:03:34 +0800

    sef

---
+++ code/main.py
@@ -0,0 +1,166 @@
+'''
+
+Data Split
+Use train_dataset and eval_dataset as train / test sets
+
+'''
+from torchvision.datasets import EMNIST
+from torch.utils.data import ConcatDataset, Subset
+from torchvision.transforms import ToTensor, Compose
+import numpy as np
+import train
+
+
+# For convenience, show image at index in dataset
+def show_image(dataset, index):
+    import matplotlib.pyplot as plt
+    plt.imshow(dataset[index][0][0], cmap=plt.get_cmap('gray'))
+
+
+def get_datasets(split='balanced', save=False):
+    download_folder = './data'
+
+    transform = Compose([ToTensor()])
+
+    dataset = ConcatDataset([EMNIST(root=download_folder, split=split, download=True, train=False, transform=transform),
+                             EMNIST(root=download_folder, split=split, download=True, train=True, transform=transform)])
+
+    # Ignore the code below with argument 'save'
+    if save:
+        random_seed = 4211  # do not change
+        n_samples = len(dataset)
+        eval_size = 0.2
+        indices = list(range(n_samples))
+        split = int(np.floor(eval_size * n_samples))
+
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+
+        train_indices, eval_indices = indices[split:], indices[:split]
+
+        # cut to half
+        train_indices = train_indices[:len(train_indices) // 2]
+        eval_indices = eval_indices[:len(eval_indices) // 2]
+
+        np.savez('train_test_split.npz', train=train_indices, test=eval_indices)
+
+    # just use save=False for students
+    # load train test split indices
+    else:
+        with np.load('./train_test_split.npz') as f:
+            train_indices = f['train']
+            eval_indices = f['test']
+
+    train_dataset = Subset(dataset, indices=train_indices)
+    eval_dataset = Subset(dataset, indices=eval_indices)
+
+    return train_dataset, eval_dataset
+
+
+# TODO 1. build your own CNN classifier with the given structure. DO NOT COPY OR USE ANY TRICK 2. load pretrained
+#  encoder from 'pretrained_encoder.pt' and build a CNN classifier on top of the encoder 3. load pretrained encoder
+#  from 'pretrained_encoder.pt' and build a Convolutional Autoencoder on top of the encoder (just need to implement
+#  decoder) *** Note that all the above tasks include implementation, training, analyzing, and reporting
+
+# example main code
+# each img has size (1, 28, 28) and each label is in {0, ..., 46}, a total of 47 classes
+if __name__ == '__main__':
+    train_ds, eval_ds = get_datasets()
+
+    img_index = 10
+    show_image(train_ds, img_index)
+    show_image(eval_ds, img_index)
+
+    ''' Show some Details about the data'''
+    print("Training set size : ", len(train_ds))
+    print("Test Set size :", len(eval_ds))
+    best_set_parameters = [32, "ADAM", 0.001]
+    tune = True
+    encoder_scratch = True
+    encoder_pretrained = True
+    decoder = True
+    train_round = 1
+
+    '''
+    a. Encoder from scratch
+    '''
+    if encoder_scratch:
+        print("---A. Encoder Scratch---")
+        if tune:
+            '''
+            1. Hold out Validation
+            '''
+            print("---1. Hold out Validation---")
+            train_set, valid_set = train.cut_validation(train_ds)
+            best_set_parameters = train.tune_encoder_params(train_set, valid_set)
+        for i in range(train_round):
+            '''
+            2. Training from Scratch
+            '''
+            print("---2. Training Entire --")
+            print("Round: ", i + 1)
+            net, predictor = train.train_encoder(train_ds, hidden_num=best_set_parameters[0], opt=best_set_parameters[1],
+                                                 learning_r=best_set_parameters[2],
+                                                 epoch=10)
+            train.test_encoder(net, predictor, train_ds, "Training")
+            '''
+            3. Testing
+            '''
+            print("---3. Testing----")
+            train.test_encoder(net, predictor, eval_ds, "Testing")
+    print("---------------------------------------------------------------------------------------------")
+    '''
+    b. Encoder from pre-trained
+    '''
+    if encoder_pretrained:
+        print("---B. Encoder Pretrained---")
+        if tune:
+            '''
+            1. Hold out Validation
+            '''
+            print("---1. Hold out Validation---")
+            train_set, valid_set = train.cut_validation(train_ds)
+            best_set_parameters = train.tune_encoder_params(train_set, valid_set,
+                                                            pre_trained_path='./pretrained_encoder.pt')
+        for i in range(train_round):
+            '''
+            2. Training from Scratch
+            '''
+            print("---2. Training Entire --")
+            print("Round: ", i + 1)
+            net, predictor = train.train_encoder(train_ds, hidden_num=best_set_parameters[0], opt=best_set_parameters[1],
+                                                 learning_r=best_set_parameters[2],
+                                                 epoch=10, pre_trained_path='./pretrained_encoder.pt')
+            train.test_encoder(net, predictor, train_ds, "Training")
+            '''
+            3. Testing
+            '''
+            print("---3. Testing----")
+            train.test_encoder(net, predictor, eval_ds, "Testing")
+    print("---------------------------------------------------------------------------------------------")
+    '''
+    c. Decoder
+    '''
+    if decoder:
+        print("---C. Decoder ---")
+        best_set_parameters = ["ADAM", 0.001]
+        if tune:
+            '''
+            1. Hold out Validation
+            '''
+            print("---1. Hold out Validation---")
+            train_set, valid_set = train.cut_validation(train_ds)
+            best_set_parameters = train.tune_decoder_params(train_set, valid_set)
+        '''
+        2. Training from Scratch
+        '''
+        print("---2. Training Entire---")
+        encoder, decoder = train.train_decoder(train_ds, opt=best_set_parameters[0],
+                                               learning_r=best_set_parameters[1],
+                                               epoch=2)
+        train.test_decoder(encoder, decoder, train_ds, "Training")
+        '''
+        3. Testing
+        '''
+        print("---3. Testing----")
+        train.test_decoder(encoder, decoder, eval_ds, "Testing")
---
+++ code/train.py
@@ -0,0 +1,258 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.utils.data
+from encoder import Encoder
+from predictor import Predictor
+from decoder import Decoder
+import random
+
+use_gpu = torch.cuda.is_available()
+use_gpu = True
+
+
+def cut_validation(data_to_cut, ratio_of_train=0.8):
+    train_size = int(ratio_of_train * len(data_to_cut))
+    test_size = len(data_to_cut) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(data_to_cut, [train_size, test_size])
+    return train_dataset, test_dataset
+
+
+def make_data_loader(data_to_loader, batch_size=16):
+    data_loader = torch.utils.data.DataLoader(data_to_loader,
+                                              batch_size=batch_size,
+                                              shuffle=True,
+                                              num_workers=16)
+    return data_loader
+
+
+def show_image(image):
+    import matplotlib.pyplot as plt
+    plt.imshow(image, cmap=plt.get_cmap('gray'))
+    plt.show()
+
+
+def tune_encoder_params(train_set, vad_set, pre_trained_path=""):
+    opts = [("ADAM", 0.001), ("SGD", 0.1), ("SGD", 0.01)]
+    hid = [32, 64]
+    epoch = 3
+    best_accuracy = -1
+    best_set_of_parameters = []
+    rounds = 0
+
+    print("Training Set Size =", len(train_set))
+    print("Validation Set Size =", len(vad_set))
+    print("Epoch =", epoch)
+
+    for hidden_num in hid:
+        for opt in opts:
+            rounds += 1
+            print("(Round " + str(rounds) + ") Parameters Details (opt,lr,hid):", opt[0],
+                  "," + str(opt[1]) + " ," + str(hidden_num))
+            net, predictor = train_encoder(train_set, epoch=epoch, learning_r=opt[1], hidden_num=hidden_num,
+                                           opt=opt[0],
+                                           pre_trained_path=pre_trained_path)
+            accuracy = test_encoder(net, predictor, vad_set)
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                best_set_of_parameters = [hidden_num, opt[0], opt[1]]
+    print("Best Hyper-parameters obtains after the hold out validation [hid,opt,lr] ")
+    print(best_set_of_parameters, "with ", best_accuracy, " of validation accuracy")
+    return best_set_of_parameters
+
+
+def test_encoder(model, predictor, vad_set, name="Validation"):
+    data_loader = make_data_loader(vad_set)
+    correct = 0
+    total = 0
+    total_loss = 0
+
+    criterion = nn.CrossEntropyLoss()
+
+    for images, labels in data_loader:
+        labels_cpu = labels
+        if use_gpu:
+            images = images.cuda()
+            labels = labels.cuda()
+
+        outputs = model(images)
+        outputs = predictor(outputs)
+
+        loss = criterion(outputs, labels)
+
+        if use_gpu:
+            _, predicted = torch.max(outputs.data.cpu(), 1)
+        else:
+            _, predicted = torch.max(outputs.data, 1)
+
+        total += labels_cpu.size(0)
+        correct += (predicted == labels_cpu).sum().item()
+        total_loss += loss.item()
+
+    print(" " + name + ' Accuracy of the model on the ' + name + ' set images: {} %'.format(100 * correct / total))
+    print("With Total Loss: ", total_loss)
+    return 100 * correct / total
+
+
+def test_decoder(encoder, decoder, vad_set, name="Validation"):
+    data_loader = make_data_loader(vad_set)
+    total_loss = 0
+    rounds = 0
+    num_show_picture = 1
+    num_show = 0
+
+    criterion = nn.MSELoss()
+
+    for images, labels in data_loader:
+        if use_gpu:
+            images = images.cuda()
+
+        rounds = rounds + 1
+        outputs = encoder(images)
+        outputs = decoder(outputs)
+
+        if num_show < num_show_picture and random.random() > 0.5:
+            num_show += 1
+            show_image(outputs.cpu().detach().numpy()[0][0])
+
+        loss = criterion(outputs, images)
+        total_loss += loss.item()
+
+    print(name + ' MSE Loss of the model on the ' + name + ' set images: {}'.format(total_loss / rounds))
+    return total_loss
+
+
+def train_encoder(train_set, hidden_num, opt, learning_r, epoch=500, batch_size=32, pre_trained_path=''):
+    data_loader = make_data_loader(data_to_loader=train_set, batch_size=batch_size)
+
+    net = Encoder()
+    predictor = Predictor(hidden_num=hidden_num)
+
+    if pre_trained_path != '':
+        net = torch.load(pre_trained_path)["model"]
+
+    if use_gpu:
+        net = net.cuda()
+        predictor = predictor.cuda()
+
+    criterion = nn.CrossEntropyLoss()
+    params = list(net.parameters()) + list(predictor.parameters())
+    if opt == 'ADAM':
+        optimizer = optim.Adam(params, lr=learning_r)
+    else:
+        optimizer = optim.SGD(params, lr=learning_r)
+
+    for epoch in range(epoch):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(data_loader, 0):
+            # get the inputs
+            inputs, labels = data
+
+            if use_gpu:
+                inputs = inputs.cuda()
+                labels = labels.cuda()
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            outputs = predictor(outputs)
+
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    return net, predictor
+
+
+def train_decoder(train_set, opt, learning_r, encoder=None, epoch=500, batch_size=32, pre_trained_path=''):
+    data_loader = make_data_loader(data_to_loader=train_set, batch_size=batch_size)
+
+    if encoder is None:
+        encoder = Encoder()
+    decoder = Decoder()
+
+    if pre_trained_path != '':
+        encoder.load_state_dict(torch.load(pre_trained_path))
+        encoder.eval()
+
+    if use_gpu:
+        decoder = decoder.cuda()
+        encoder = encoder.cuda()
+
+    criterion = nn.MSELoss()
+    params = list(decoder.parameters()) + list(encoder.parameters())
+
+    if opt == "ADAM":
+
+        optimizer = optim.Adam(params, lr=learning_r)
+    else:
+        optimizer = optim.SGD(params, lr=learning_r)
+
+    for epoch in range(epoch):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(data_loader, 0):
+            # get the inputs
+            inputs, labels = data
+
+            if use_gpu:
+                inputs = inputs.cuda()
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            hidden = encoder(inputs)
+            outputs = decoder(hidden)
+
+            loss = criterion(outputs, inputs)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    return encoder, decoder
+
+
+def tune_decoder_params(train_set, vad_set):
+    opts = [("ADAM", 0.001), ("SGD", 0.1), ("SGD", 0.01)]
+    epoch = 3
+    best_accuracy = -1
+    best_set_of_parameters = []
+    rounds = 0
+
+    print("---Parameters Tuning---")
+    print("Training Set Size = ", len(train_set))
+    print("Validation Set Size = ", len(vad_set))
+    print("Epoch =", epoch)
+
+    for opt in opts:
+        rounds += 1
+        print("(Round " + str(rounds) + ") Parameters Details (opt,lr):", opt[0],
+              ", " + str(opt[1]))
+        encoder, decoder = train_decoder(train_set, epoch=epoch, learning_r=opt[1],
+                                         opt=opt[0],
+                                         pre_trained_path='')
+        accuracy = test_decoder(encoder, decoder, vad_set)
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_set_of_parameters = [opt[0], opt[1]]
+    print("Best Hyper-parameters obtains after the hold out validation [hid,opt,lr] ")
+    print(best_set_of_parameters)
+    print("with ", best_accuracy, " of validation accuracy")
+    return best_set_of_parameters
